9b00e5a9cf7a092d503a6e622ffbe089f7a1fe30
TechWithTwin <realwebtechguru@gmail.com>
2025-05-14 17:19:06 +0300

    adding logo placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <Link href="/admin">
-      <Button>Chakra admin Dashboard Click to visit</Button>
-    </Link>
+    <Flex h="100vh" w="100vw" align="center" justify="center">
+      <Stack align="center">
+        <Link href="/admin">
+          <Button colorPalette="red" size="xl">
+            Chakra admin Dashboard Click to visit
+          </Button>
+        </Link>
+        <a href="https://github.com/techwithtwin" target="_blank">
+          <Button>
+            © TechWithTwin <FaGithub />
+          </Button>
+        </a>
+      </Stack>
+    </Flex>
   );
 }
